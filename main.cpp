fd0d0378341b3889de369a8bd127baa9df1c679a
DuParkLinux <dupark3@gmail.com>
2018-05-21 11:45:26 -0400

    Cleaned up output during game

--- main.cpp
+++ main.cpp
@@ -36,30 +36,26 @@ int main(){
     while (true){
         // player1's turn. input from user is electron config. 
         // store atomic number and element symbol for convenience as local variables
-        cout << "It is " << player1 << "'s turn to take a shot with an "
-             << "electron configuration: ";
+        cout << player1 << "'s turn to take a shot with an electron configuration: ";
         string electron_config;
         cin >> electron_config;
         int atomic_number = electron_configs_reverse[electron_config];
         string element_symbol = element_symbols[atomic_number];
 
-
         if(computer.check_shot(electron_config)){
-            cout << "HIT! Element " << element_symbol << " has been shot down." << endl;
+            cout << player1 << " HIT! Element " << element_symbol << " has been shot down." << endl;
+            if (computer.check_game_over()){
+                cout << "********GAME OVER, " << player1 << " IS VICTORIOUS**********" << endl;
+                break;
+            }
         } else if (atomic_number == 0) {
-            cout << "MISFIRE! Electron config " << electron_config << " is incorrect." << endl;
+            cout << player1 << " MISFIRE! Electron config " << electron_config << " is incorrect." << endl;
         } else {
-            cout << "MISS! Element " << element_symbol << " is open waters." << endl;
-        }
-        
-        
-        if (computer.check_game_over()){
-            cout << "********GAME OVER, " << player1 << " IS VICTORIOUS**********" << endl;
-            break;
+            cout << player1 << " MISS! Element " << element_symbol << " is open waters." << endl;
         }
         
         // Player2's turn
-        cout << endl << "It is " << player2 << "'s turn to take a shot with an electron configuration: ";
+        cout << player2 << "'s turn to take a shot with an electron configuration: ";
 
         electron_config = electron_configs[rand() % 18 + 1];
         cout << electron_config << endl;
@@ -67,17 +63,18 @@ int main(){
         element_symbol = element_symbols[atomic_number];
 
         if (user.check_shot(electron_config)){
-            cout << "HIT! Element " << element_symbol << " has been shot down." << endl;
-        } else if (atomic_number == 0) {
-            cout << "MISFIRE! Electron config " << electron_config << " is incorrect." << endl;
-        } else {
-            cout << "MISS! Element " << element_symbol << " is open waters." << endl;
-        }
-
-        if (user.check_game_over()){
+            cout << player2 << " HIT! Element " << element_symbol << " has been shot down." << endl;
+            if (user.check_game_over()){
             cout << "********GAME OVER, " << player2 << " IS VICTORIOUS**********" << endl;
             break;
         }
+        } else if (atomic_number == 0) {
+            cout << player2 << " MISFIRE! Electron config " << electron_config << " is incorrect." << endl;
+        } else {
+            cout << player2 <<  " MISS! Element " << element_symbol << " is open waters." << endl;
+        }
+
+        
     }
 
 
