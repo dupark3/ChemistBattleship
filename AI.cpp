138a03622353f62685f1748bc59d489b832cd66e
DuParkLinux <dupark3@gmail.com>
2018-05-30 09:50:03 -0400

    take_educated_shot() now stores a vector of element_nodes* that have maximum possibilities. Returns a random elec_config from the container's nodes

--- AI.cpp
+++ AI.cpp
@@ -11,14 +11,17 @@ using namespace std;
 
 string AI::take_educated_shot(const player& player1){
     int max_possibility = 0;
-    element_node* best_element_to_shoot;
+    vector<element_node*> best_elements_to_shoot;
     for (int i = 1; i != 119; ++i){
         if (element_node_array[i]->possibilities > max_possibility){
             max_possibility = element_node_array[i]->possibilities;
-            best_element_to_shoot = element_node_array[i];
+            best_elements_to_shoot.clear();
+            best_elements_to_shoot.push_back(element_node_array[i]);
+        } else if (element_node_array[i]->possibilities == max_possibility) {
+            best_elements_to_shoot.push_back(element_node_array[i]);
         }
     }
-
-    return best_element_to_shoot->get_electron_config();
+    int rand = my_rand(best_elements_to_shoot.size());
+    return best_elements_to_shoot[rand]->get_electron_config();
     
 }
